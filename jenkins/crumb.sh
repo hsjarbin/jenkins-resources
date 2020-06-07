fd963294beb3ad78d2e9cb160321441b4d4a40ba
hsjarbin <37246147+hsjarbin@users.noreply.github.com>
2020-06-07 08:38:35 +0700

    Update crumb.sh

--- jenkins/crumb.sh
+++ jenkins/crumb.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 crumb=$(curl -u "jenkins:1234" -s 'http://jenkins.ocp.abip:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 curl -u "jenkins:1234" -H "$crumb" -X POST http://jenkins.ocp.abip:8080/job/ENV/build?delay=0sec
 curl -u "jenkins:1234" -H "$crumb" -X POST  http://jenkins.ocp.abip:8080/job/backup-to-aws-s3/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=abip-jenkins-mysql-backup
